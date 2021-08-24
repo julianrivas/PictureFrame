f8e6d0d31cee091371a940c3f4ee06db9f2a83e1
Julian Rivas <Juliandarivas@hotmail.com>
2021-08-23 21:50:00 -0500

    ClickAndDragHandler deleted

--- Presentation/PictureFrame.cs
+++ Presentation/PictureFrame.cs
@@ -1,7 +1,4 @@
 ﻿using Presentation.Handlers;
-using Presentation.Main;
-using Presentation.Properties;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -9,37 +6,25 @@ namespace Presentation
 {
     public partial class PictureFrame : Form 
     {
-        private PictureFrameRenderer _pictureFrameRenderer;
-
-        private readonly List<Bitmap> images = new()
-        {
-            Resources.IMG_143949244608458,
-            Resources.IMG_132060135311861,
-            Resources.IMG_241369506629628,
-            Resources.IMG_249997204594106,
-            Resources.IMG_249595061164167,
-            Resources.IMG_44353119378146,
-            Resources.IMG_87564803925760,
-            Resources.IMG_44243566241063
-        };
+        private Point _lastPoint;
 
         public PictureFrame()
         {
             InitializeComponent();
-            _pictureFrameRenderer = new PictureFrameRenderer();
         }
-
 
         private void PictureFrame_MouseMove(object sender, MouseEventArgs e)
         {
-            ClickAndDragHandler.MouseMove(sender, e);
-            Left = ClickAndDragHandler.Left;
-            Top = ClickAndDragHandler.Top;
+            if (e.Button == MouseButtons.Left)
+            {
+                Left += e.X - _lastPoint.X;
+                Top += e.Y - _lastPoint.Y;
+            }
         }
 
         private void PictureFrame_MouseDown(object sender, MouseEventArgs e)
         {
-            ClickAndDragHandler.MouseDown(sender, e);
+            _lastPoint = new Point(e.X, e.Y);
         }
     }
 }
